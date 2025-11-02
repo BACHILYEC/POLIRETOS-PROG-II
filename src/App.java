44e0afab85cde710594b93a7712bf0d384d280fa
BachilyPC <adrian.munoz01@gmail.com>
2025-11-01 23:45:19 -0500

    Finalizado

--- src/App.java
+++ src/App.java
@@ -1,11 +1,14 @@
+import Compilador.ejercicio.numerodecimal;
 import Recursividad.ControllerRecursividad;
 import SerieNumerica.ControllerSerie;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        ControllerRecursividad controller = new ControllerRecursividad();
-        controller.Ejercicio();
-        ControllerSerie controllerSerie = new ControllerSerie();
-        controllerSerie.Ejercicio();
+        // ControllerRecursividad controller = new ControllerRecursividad();
+        // controller.Ejercicio();
+        // ControllerSerie controllerSerie = new ControllerSerie();
+        // controllerSerie.Ejercicio();
+        numerodecimal numero = new numerodecimal("");
+        numero.decimal();
     }
 }
