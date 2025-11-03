c2e948f1b566bee7a334dea711ade631a6b238e1
Pandy_06 <guarany666@gmail.com>
2025-11-03 11:13:13 -0500

    Carga nombre

--- src/Arrays/Variablesandy.java
+++ src/Arrays/Variablesandy.java
@@ -1,5 +1,26 @@
 package Arrays;
+import java.util.Scanner;
 
 public class Variablesandy {
+    public String nombreCompleto;
+    public String[] palabras;
+    public int[] porcentaje;
 
+    public void pedirDatos() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese su nombre completo: ");
+        nombreCompleto = sc.nextLine();
+
+        palabras = nombreCompleto.split(" ");
+        porcentaje = new int[palabras.length];
+
+        System.out.println("\nIngrese el porcentaje de carga para cada palabra:");
+        for (int i = 0; i < palabras.length; i++) {
+            System.out.print(palabras[i] + ": ");
+            porcentaje[i] = sc.nextInt();
+        }
+
+        sc.close();
+    }
 }
--- src/Arrays/ejerciciosandy/CargaNombre.java
+++ src/Arrays/ejerciciosandy/CargaNombre.java
@@ -1,5 +1,62 @@
 package Arrays.ejerciciosandy;
 
-public class CargaNombre {
+import Arrays.Variablesandy;
 
+//Codigo utilizando for
+public class CargaNombre {
+    public static void main(String[] args) {
+        Variablesandy datos = new Variablesandy();
+        datos.pedirDatos();
+
+        for (int i = 0; i < datos.palabras.length; i++) {
+            int longitudBarra = (int) Math.round((datos.porcentaje[i] / 100.0) * 12);
+            int longitudNombre = (int) Math.round((datos.porcentaje[i] / 100.0) * datos.palabras[i].length());
+
+            if (longitudBarra < 1 && datos.porcentaje[i] > 0) longitudBarra = 1;
+            if (longitudNombre < 1 && datos.porcentaje[i] > 0) longitudNombre = 1;
+
+            String barra = "=".repeat(longitudBarra);
+            String parte = datos.palabras[i].substring(0, longitudNombre);
+
+            System.out.printf("$$%-12s>$$ %3d%% %s%n", barra, datos.porcentaje[i], parte);
+        }
+            System.out.printf("\n");
+
+
+//Codigo utilizando while
+
+        int i = 0;
+        while (i < datos.palabras.length) {
+            int longitudBarra = (int) Math.round((datos.porcentaje[i] / 100.0) * 12);
+            int longitudNombre = (int) Math.round((datos.porcentaje[i] / 100.0) * datos.palabras[i].length());
+
+            if (longitudBarra < 1 && datos.porcentaje[i] > 0) longitudBarra = 1;
+            if (longitudNombre < 1 && datos.porcentaje[i] > 0) longitudNombre = 1;
+
+            String barra = "=".repeat(longitudBarra);
+            String parte = datos.palabras[i].substring(0, longitudNombre);
+
+            System.out.printf("$$%-12s>$$ %3d%% %s%n", barra, datos.porcentaje[i], parte);
+
+            i++;
+        }
+            System.out.printf("\n");
+
+        //Codigo utilizando do...while
+        i=0;
+        do {
+            int longitudBarra = (int) Math.round((datos.porcentaje[i] / 100.0) * 12);
+            int longitudNombre = (int) Math.round((datos.porcentaje[i] / 100.0) * datos.palabras[i].length());
+
+            if (longitudBarra < 1 && datos.porcentaje[i] > 0) longitudBarra = 1;
+            if (longitudNombre < 1 && datos.porcentaje[i] > 0) longitudNombre = 1;
+
+            String barra = "=".repeat(longitudBarra);
+            String parte = datos.palabras[i].substring(0, longitudNombre);
+
+            System.out.printf("$$%-12s>$$ %3d%% %s%n", barra, datos.porcentaje[i], parte);
+
+            i++;
+        } while (i < datos.palabras.length);
+    }
 }
