fe89af2653056c093ef968f827ece8f1e6ea43e8
KarliiBoo <karli_cruz29@hotmail.com>
2025-11-09 18:02:14 -0500

    Añadiendo

---
+++ App.java
@@ -0,0 +1,9 @@
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        Controlador controlador = new Controlador();
+        controlador.mostrarPortada();
+        controlador.Ejercicio();
+    }
+
+}
